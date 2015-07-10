d7e0856b7872d8522065618cadd4fae65bc68d83
supraja9 <supraja.vattikundala@neevtech.com>
2015-07-10 16:08:11 +0530

    cleanup

--- popeyetires/core/src/main/java/com/popeyetires/popeyetires/core/utils/LocaleUtil.java
+++ popeyetires/core/src/main/java/com/popeyetires/popeyetires/core/utils/LocaleUtil.java
@@ -5,13 +5,11 @@ import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUse;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 
 public class LocaleUtil extends WCMUse{
 
 	private final Logger logger = LoggerFactory.getLogger(LocaleUtil.class);
 	private Page page;
-	private PageManager pageManager;
     private String path;
     private String locale = "";
 	@Override
@@ -19,7 +17,6 @@ public class LocaleUtil extends WCMUse{
 	  logger.info("in actiave");
 	  try {
 		  page = super.getCurrentPage();
-	        pageManager = super.getPageManager();
 	        path = page.getPath();
 	        logger.info("in path:::::>>>"+ path); 
 	        if(path.contains("/en/us/")) {
