d0306db628bd769dd87bba7e30a9f6e509e322c2
ramanareddy1234 <ramanareddy.moole@razorfish.com>
2015-07-13 12:41:55 +0530

    Adding Tire Information realted code

--- popeyetires/core/src/main/java/com/popeyetires/popeyetires/core/models/TireInfo.java
+++ popeyetires/core/src/main/java/com/popeyetires/popeyetires/core/models/TireInfo.java
@@ -12,20 +12,40 @@ public class TireInfo {
 		return title;
 	}
 	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public String getDescription() {
 		return description;
 	}
 	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public String getTreadDepth() {
 		return treadDepth;
+	}
+	
+	public void setTreadDepth(String treadDepth) {
+		this.treadDepth = treadDepth;
 	}
 	
 	public String getWarranty() {
 		return warranty;
 	}
 	
+	public void setWarranty(String warranty) {
+		this.warranty = warranty;
+	}
+	
 	public String getPrice() {
 		return price;
+	}
+	
+	public void setPrice(String price) {
+		this.price = price;
 	}
 
 	public String getTireImage() {
@@ -34,5 +54,9 @@ public class TireInfo {
 
 	public void setTireImage(String tireImage) {
 		this.tireImage = tireImage;
-	}	
+	}
+	
+	public String toString() {
+		return "title=[" + title + "]description=[" + description + "]treadDepth=[" + treadDepth + "]warranty=[" + warranty + "]price=[" + price + "]";
+	}
 }
